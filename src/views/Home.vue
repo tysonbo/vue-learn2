ffc3eb42d8479092c5407efbbac15917a78a8e99
unknown <tyson_c_bodager@progressive.com>
2020-01-16 13:34:03 -0500

    added listeners

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div style="color:red; font-size: x-large" id="info">Info Div</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
 
-    <div style="color:red; font-size: x-large" id="info">Info Div</div>
+
   </div>
 </template>
 
